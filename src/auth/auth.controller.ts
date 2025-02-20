4f8078b6d17e6eafc38234ec01fd8721f48ddc80
gabriel-gal <gabriel.lopes.gal@gmail.com>
2025-02-20 17:12:45 -0300

    feaat(auth): implementacao do login JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+    constructor(private readonly authService: AuthService) { }
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
-    login(){
-        return "Login feito"
-        //return this.authService.login();
+    login(@Request() req: AuthRequest) {
+        return this.authService.login(req.user);
     }
 }
